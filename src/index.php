c723cb6c8fa1cd7617694058e523bbcb8dc3f091
Ythildyr <pivardjulien@gmail.com>
2017-07-29 16:09:25 +0200

    Réorganisation des sources

--- src/index.php
+++ src/index.php
@@ -1,12 +1,17 @@
 <?php
+
+// pour le php7
+// Cette déclaration doit être mise au début de chaque fichier
+declare( strict_types = 1 );
+
 session_start();
+
 ini_set("display_errors",1);
 ini_set("log_errors",1);
+
 error_reporting(E_ALL);
+
 require("entree_josselin.php");
 require("entree_julien.php");
-
-
-
 
 ?>
