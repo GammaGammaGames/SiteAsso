68a4ea0033a4f443baf6eba3e22e82a0b0910c8f
Ythildyr <pivardjulien@gmail.com>
2017-08-04 13:43:13 +0200

    Test de la configuration restfull

--- src/entree_julien.php
+++ src/entree_julien.php
@@ -1,1 +1,42 @@
 <?php
+require_once("/var/private/mysql_config.php");
+
+echo ("<p>");
+echo ("Driver disponible avec pdo : ");
+print_r(PDO::getAvailableDrivers());
+echo ("</p>");
+
+try
+{
+    $pdo = new PDO('mysql:host='
+        . MYSQL_HOST
+        . ';port='
+        . MYSQL_PORT
+        . ';dbname='
+        . MYSQL_DB
+        . ';charset=utf8'
+        , MYSQL_USER, MYSQL_PASSWORD);
+
+    $r = "SELECT * FROM TEST";
+
+    $s = $pdo->prepare( $r );
+    $s->execute();
+
+    echo ("<p>");
+    var_dump ($s->fetchAll());
+    echo ("</p>");
+}
+catch(PDOException $e) {
+    $msg = 'ERREUR PDO dans ' . $e->getFile() . ' L.' . $e->getLine() . ' : ' . $e->getMessage();
+    die($msg);
+}
+
+echo ("<p>");
+var_dump ($_GET);
+echo ("</p>");
+echo ("<p>");
+var_dump ($_POST);
+echo ("</p>");
+echo ("<p>");
+var_dump ($_REQUEST);
+echo ("</p>");
