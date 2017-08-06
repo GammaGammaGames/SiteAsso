1cbf4a061a619bcf64405277600ea2d5f9f5b5fe
Toedaem <josselin.gueneron@laposte.net>
2017-08-06 21:50:23 +0200

    Test sur la bdd

--- src/entree_josselin.php
+++ src/entree_josselin.php
@@ -1,6 +1,25 @@
 <?php
+require_once("/var/private/mysql_config.php");
 
-echo "cacaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa ! le pipi c'est nul !";
+$pdo = new PDO('mysql:host='
+    . MYSQL_HOST
+    . ';port='
+    . MYSQL_PORT
+    .';dbname='
+    . MYSQL_DB
+    . ';charset=utf8'
+    , MYSQL_USER, MYSQL_PASSWORD);
+
+$req_test=$pdo->query('SELECT * FROM joueurs;');
+//$req_test->setFetchMode(PDO::FETCH_ASSOC);
+$req_test->setFetchMode(PDO::FETCH_OBJ);
+while($line = $req_test->fetch()){
+    echo "<p>";
+    var_dump($line);
+    echo "</p>";
+}
+//var_dump($res_test);
+echo "fini";
 
 ?>
 
