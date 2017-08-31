04f5982c303cc3f3898694b31bc55862624e0b36
Ythildyr <pivardjulien@gmail.com>
2017-08-31 18:58:57 +0200

    Typo bdd tests

--- bdd/test.sql
+++ bdd/test.sql
@@ -8,15 +8,15 @@ CREATE TABLE IF NOT EXISTS Test
 );
 
 INSERT INTO Test ( `nom`, `categorie`, `quantite` ) VALUES
-( 'CocaNasson'               , 'Boisson' , 104 ) ,
-( 'OrangiFruits'             , 'Boisson' , 222 ) ,
-( 'Slurm'                    , 'Boisson' , 200 ) ,
-( 'SoleilVert Cola'          , 'Boisson' , 50  ) ,
-( 'Olde Fortran'             , 'Alcool'  , 394 ) ,
-( 'Chips Glagnard à lhumain' , 'En-Cas'  , 573 ) ,
-( 'Popplers'                 , 'En-Cas'  , 44  ) ,
-( 'Amiral Crunch'            , 'En-Cas'  , 95  ) ,
-( 'Pamplemouse beaconisé'    , 'En-Cas'  , 32  ) ,
-( 'Sandwich jambon beurre'   , 'En-Cas'  , 433 ) ,
-( 'Burger'                   , 'En-Cas'  , 200 ) ,
-( 'Bashelor shew'            , 'En-Cas'  , 89  );
+( 'CocaNasson'                 , 'Boisson' , 104 ) ,
+( 'OrangiFruits'               , 'Boisson' , 222 ) ,
+( 'Slurm'                      , 'Boisson' , 200 ) ,
+( 'SoleilVert Cola'            , 'Boisson' , 50  ) ,
+( 'Olde Fortran'               , 'Alcool'  , 394 ) ,
+( 'Chips Glagnard à l\'humain' , 'En-Cas'  , 573 ) ,
+( 'Popplers'                   , 'En-Cas'  , 44  ) ,
+( 'Amiral Crunch'              , 'En-Cas'  , 95  ) ,
+( 'Pamplemouse beaconisé'      , 'En-Cas'  , 32  ) ,
+( 'Sandwich jambon beurre'     , 'En-Cas'  , 433 ) ,
+( 'Burger'                     , 'En-Cas'  , 200 ) ,
+( 'Bashelor shew'              , 'En-Cas'  , 89  );
